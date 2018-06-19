1a8fe425180e8780d0555508031248ec18236ff6
ADETEL99\jbenethuiliere <jbenethuiliere@witekio.com>
2018-06-19 18:15:25 +0200

    add settings

--- src/settings.ts
+++ src/settings.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 export const Settings = {
     port: 3000,
     version: "1.0.0",
-    host: 'http://192.168.1.24',
+    // host: 'http://192.168.1.24',
+	host: 'http://10.69.1.148',
     rootDir: path.resolve(__dirname)
 }
